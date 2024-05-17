88b08283c5e5b3a1edce3c75a02ff9848ab08c02
KakaChristian <christ123461@gmail.com>
2024-05-17 18:27:15 +0800

    Perubahan pada notifikasi dudi menjadi menggunakan sliver

--- lib/app/modules/dudi/notifikasi_dudi/views/notifikasi_dudi_view.dart
+++ lib/app/modules/dudi/notifikasi_dudi/views/notifikasi_dudi_view.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: must_be_immutable
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simon_pkl/material/material.dart';
@@ -14,86 +12,74 @@ class NotifikasiDudiView extends GetView<NotifikasiDudiController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AllMaterial.colorWhite,
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            ClipPath(
-              clipper: ClipPathClass(),
-              child: Container(
-                height: 300,
-                width: Get.width,
-                color: AllMaterial.colorBlue,
-                child: SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Stack(
               children: [
-                Padding(
-                  padding:
-                      EdgeInsets.only(top: context.mediaQueryPadding.top + 20),
-                  child: Text(
-                    "Notifikasi",
-                    style: TextStyle(
-                      fontFamily: AllMaterial.fontFamily,
-                      color: AllMaterial.colorWhite,
-                      fontWeight: AllMaterial.fontSemiBold,
-                      fontSize: 18,
+                ClipPath(
+                  clipper: ClipPathClass(),
+                  child: Container(
+                    height: 300,
+                    width: Get.width,
+                    color: AllMaterial.colorBlue,
+                    child: SafeArea(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Notifikasi",
+                                style: TextStyle(
+                                  fontFamily: AllMaterial.fontFamily,
+                                  color: AllMaterial.colorWhite,
+                                  fontWeight: AllMaterial.fontSemiBold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              Container(
+                                alignment: Alignment.centerRight,
+                                // margin: EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(
+                                  Icons.notifications,
+                                  size: 200,
+                                  color: AllMaterial.colorWhite.withOpacity(.3),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.access_time_sharp,
+                                color: AllMaterial.colorWhite,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                "Pesan akan terhapus dalam 24 Jam terakhir",
+                                style: TextStyle(
+                                  fontFamily: AllMaterial.fontFamily,
+                                  fontSize: 11,
+                                  color: AllMaterial.colorWhite,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  alignment: Alignment.centerRight,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 30,
-                  ),
-                  child: Icon(
-                    Icons.notifications,
-                    size: 210,
-                    color: AllMaterial.colorWhite.withOpacity(.5),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 230, horizontal: 40),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.access_time_sharp,
-                    color: AllMaterial.colorWhite,
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    "Pesan akan terhapus dalam 24 Jam terakhir",
-                    style: TextStyle(
-                      fontFamily: AllMaterial.fontFamily,
-                      fontSize: 11,
-                      color: AllMaterial.colorWhite,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Column(
-              children: [
-                SizedBox(
-                  height: Get.height * 0.33,
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  padding: EdgeInsets.only(
+                    right: 50,
+                    left: 50,
+                    top: context.mediaQueryPadding.top * 10.5,
+                    bottom: 30,
+                  ),
                   child: Container(
                     height: 55,
                     width: Get.width,
@@ -113,7 +99,7 @@ class NotifikasiDudiView extends GetView<NotifikasiDudiController> {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Padding(
-                          padding: const EdgeInsets.only(left: 30),
+                          padding: const EdgeInsets.only(left: 10),
                           child: Text(
                             "Notifikasi : ",
                             style: TextStyle(
@@ -140,54 +126,52 @@ class NotifikasiDudiView extends GetView<NotifikasiDudiController> {
                             ),
                           ),
                         ),
-                        // SizedBox(width: 10),
                       ],
                     ),
                   ),
                 ),
-                // SizedBox(height: 20),
-                notif == 0
-                    ? Center(
-                        heightFactor: 13,
-                        child: Text(
-                          "Belum ada pesan",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: AllMaterial.fontFamily,
-                            fontSize: 20,
-                            color: AllMaterial.colorGrey.withOpacity(.4),
-                          ),
-                        ),
-                      )
-                    : Padding(
-                        padding: const EdgeInsets.only(
-                            bottom: 10, left: 10, right: 10),
-                        child: SizedBox(
-                          height: Get.height,
-                          child: ListView.builder(
-                              itemCount: notif,
-                              itemBuilder: (context, index) {
-                                return Column(
-                                  children: [
-                                    NotifikasiItemDudi(
-                                      contextTitle: "Kabar baik untukmu!",
-                                      subTitle:
-                                          "Ajuan PKL-mu diterima di PT. Telkom Indonesia",
-                                      contextImage: "assets/logo/accept.png",
-                                      onTapFunc: () {
-                                        // Get.toNamed(RoutesName.settingsPage);
-                                      },
-                                    ),
-                                    // SizedBox(height: 20),
-                                  ],
-                                );
-                              }),
-                        ),
-                      ),
               ],
             ),
-          ],
-        ),
+          ),
+          notif == 0
+              ? SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 40),
+                      child: Text(
+                        "Belum ada pesan",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: AllMaterial.fontFamily,
+                          fontSize: 20,
+                          color: AllMaterial.colorGrey.withOpacity(.4),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              : SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return Column(
+                        children: [
+                          NotifikasiItemDudi(
+                            contextTitle: "Kabar baik untukmu!",
+                            subTitle:
+                                "Ajuan PKL-mu diterima di PT. Telkom Indonesia",
+                            contextImage: "assets/logo/accept.png",
+                            onTapFunc: () {
+                              // Get.toNamed(RoutesName.settingsPage);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                    childCount: notif,
+                  ),
+                ),
+        ],
       ),
     );
   }
